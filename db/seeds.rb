0dbe62241ea39cb885ce383affeccd352814bc87
guillaume veldekens <guillaume.veldekens@gmail.com>
2020-02-19 12:16:57 +0100

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'belgian',
+    phone_number: '0399288292'
+  },
+    {
+    name:         'Paddo',
+    address:      'paddostreet',
+    category:  'italian',
+    phone_number: '03003200092'
+  },
+    {
+    name:         'Istanbul',
+    address:      'turkeystreet 7',
+    category:  'italian',
+    phone_number: '0876888292'
+  },
+    {
+    name:         'Tokiyooo',
+    address:      'chinatown',
+    category:  'japanese',
+    phone_number: '0399288292'
+  },
+    {
+    name:         'Le Nimporte',
+    address:      'parisstreet 8',
+    category:  'belgian',
+    phone_number: '0399288292'
+  }
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
